0773ecaf1adca907d55315992e598baef390073f
Schadrack2544 <nzayinaschad250@gmail.com>
2022-10-13 15:29:01 +0200

    Adding poppins font-family.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { Body, Navbar, Footer } from '../components'
+import "@fontsource/poppins";
 
 const Home: NextPage = () => {
   return (
